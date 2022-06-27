a45002ea02480c6d0782c897219ae91ed201e3e5
crashRT <crashrt.farbauti@gmail.com>
2022-06-28 08:01:06 +0900

    サービスコンテナのコメントを追記

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -15,7 +15,7 @@ class IndexController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request, TweetService $tweetService)
+    public function __invoke(Request $request, TweetService $tweetService) // TweetService を受け取るようにした
     {
         // $tweets = Tweet::orderBy('created_at', 'DESC')->get();
         //$tweetService = new TweetService(); // TweetService のインスタンスを作成
