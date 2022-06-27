866885f19f8aed42c2a4a3b453f03388efd8095b
crashRT <crashrt.farbauti@gmail.com>
2022-06-28 07:45:01 +0900

    サービスコンテナを使うようにした

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Tweet;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Tweet;
+use App\Services\TweetService;
 
 class IndexController extends Controller
 {
@@ -14,10 +15,12 @@ class IndexController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, TweetService $tweetService)
     {
-        $tweets = Tweet::orderBy('created_at', 'DESC')->get();
-        
+        // $tweets = Tweet::orderBy('created_at', 'DESC')->get();
+        //$tweetService = new TweetService(); // TweetService のインスタンスを作成
+        $tweets = $tweetService -> getTweets();  
+
         return view('tweet.index')
             -> with('tweets', $tweets);
     }
